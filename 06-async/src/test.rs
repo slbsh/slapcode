0e7055919f9b5d142903ca69611f213694bc3b46
slbsh <crafter.antek@gmail.com>
2025-01-22 17:11:04 +0100

    fix: update 06-async tests

--- 06-async/src/test.rs
+++ 06-async/src/test.rs
@@ -1,27 +1,47 @@
 use crate::Runtime;
+use std::sync::atomic::{AtomicU8, AtomicBool, Ordering};
 
 #[test]
 fn test() {
-	Runtime::spawn_blocking(async { println!("Hello, world!"); });
+	static FLAG: AtomicBool = AtomicBool::new(false);
+
+	Runtime::spawn_blocking(async { 
+		println!("Hello, world!");
+		FLAG.store(true, Ordering::Relaxed);
+	});
+
+	assert_eq!(FLAG.load(Ordering::Relaxed), true);
 }
 
 #[test]
 fn test2() {
+	static FLAG: AtomicBool = AtomicBool::new(false);
+
 	async fn foo() {
 		println!("ur async smells");
+		std::thread::sleep(std::time::Duration::from_secs(1));
+		FLAG.store(true, Ordering::Relaxed);
 	}
 
 	Runtime::spawn_blocking(foo());
+
+	assert_eq!(FLAG.load(Ordering::Relaxed), true);
 }
 
 #[test]
 fn test3() {
+	static COUNT: AtomicU8 = AtomicU8::new(0);
+
 	async fn foo() {
+		COUNT.fetch_add(1, Ordering::Relaxed);
 		println!("ur async smells");
 	}
 
 	Runtime::spawn_blocking(async {
 		println!("Hello, world!");
+		COUNT.fetch_add(1, Ordering::Relaxed);
 		foo().await;
 	});
+
+	assert_eq!(COUNT.load(Ordering::Relaxed), 2);
 }
